28693c53c1c8423c603cf02b06a9ffdd2762be86
Ryan Gautier <ryangautier2@gmail.com>
2020-04-17 16:01:47 -0500

    saved notes are overwritten by user data

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 // set up express
 var app = express();
 var PORT = 5050;
+app.use(express.static(__dirname + '/public'));
 
 // data parsing in express
 app.use(express.urlencoded({ extended: true }));
@@ -12,27 +13,36 @@ app.use(express.json());
 
 // Routes
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // API requests
-app.get("/api/notes", function(req, res) {
-    fs.readFile("db/db.json", "utf8", function(err, data) {
-        if (err){
+app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", "utf8", function (err, data) {
+        if (err) {
             return error(err);
         }
-        return data;
+        return res.json(JSON.parse(data));
     });
-    return res.json(jsondata);
+});
+
+app.post("/api/notes", function (req, res) {
+    var updatedNotes = (req.body);
+    console.log(updatedNotes);
+    fs.writeFile("db/db.json", JSON.stringify(updatedNotes), function (err) {
+        if (err) {
+            error(err);
+        }
+    });
 });
 
 
 // start server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on port " + PORT);
 });
